9444e36849b5e8edda8b85282933d8cfe1dbb3ac
clairemelzer <claire-melzer@uiowa.edi>
2015-11-01 22:25:30 +0000

    created the basic contoller methods for building

---
+++ apartments/app/controllers/buildings_controller.rb
@@ -0,0 +1,45 @@
+class BuildingsController < ApplicationController
+
+  def building_params
+    params.require(:building).permit(:address, :management)
+  end
+
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @building = Building.find(id) # look up movie by unique ID
+
+  end
+
+  def index
+    @building = Building.all
+  end
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+    @building = Building.create!(building_params)
+    flash[:notice] = "#{@building.address} was successfully created."
+    redirect_to buildings_path
+  end
+
+  def edit
+    @building = Building.find params[:id]
+  end
+
+  def update
+    @building = Building.find params[:id]
+    @building.update_attributes!(building_params)
+    flash[:notice] = "#{@building.address} was successfully updated."
+    redirect_to buildings_path(@building)
+  end
+
+  def destroy
+    @building = Building.find(params[:id])
+    @building.destroy
+    flash[:notice] = "Building '#{@building.address}' deleted."
+    redirect_to buildings_path
+  end
+
+end
