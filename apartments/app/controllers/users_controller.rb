81c8dda5c89f3d4179fc5fb5d0f7b823d3f24db3
mfina94 <matthawkeyefina@gmail.com>
2015-11-03 15:36:12 +0000

    user had to confirm password on signup

--- apartments/app/controllers/users_controller.rb
+++ apartments/app/controllers/users_controller.rb
@@ -10,14 +10,18 @@ class UsersController < ApplicationController
     def create
         email = params[:user][:email]
         if (User.where(:email => email).blank?)
-            @temp = User.new
-            @user = @temp.create_user user_params
-            flash[:notice]= "Welcome #{@user.name} your username was successfully created."
-            redirect_to buildings_path
+            if params[:user][:password] == params[:user][:password2]
+                @temp = User.new
+                @user = @temp.create_user user_params
+                flash[:notice]= "Welcome #{@user.name} your username was successfully created."
+                redirect_to buildings_path
+            else
+                flash[:notice] = "You need to confirm your password"
+                redirect_to :back
+            end
         else
             flash[:notice] = "Sorry, this email is already taken. Try a different one"
             redirect_to :back
-            
         end
     end
 end
