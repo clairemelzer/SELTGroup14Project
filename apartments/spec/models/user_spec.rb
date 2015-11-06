ffef2e4c1ece8980bb057fe3ec9a84a5fc73e773
jzlantz <jzlantz@gmail.com>
2015-11-06 03:51:21 +0000

    Add validation Rspec for all user attributes

--- apartments/spec/models/user_spec.rb
+++ apartments/spec/models/user_spec.rb
@@ -11,6 +11,13 @@ RSpec.describe User, type: :model do
  it "Is invalid without password" do
      expect(build(:user, password: nil)).to_not be_valid
  end
+  it "Is invalid without password  confirmation" do
+     expect(build(:user, password_confirmation: nil)).to_not be_valid
+ end
+  it "Is invalid without name" do
+     expect(build(:user, name: nil)).to_not be_valid
+ end
+ 
  it "creates a session token" do
      user = create(:user)
      expect(user.session_token).to be_truthy
