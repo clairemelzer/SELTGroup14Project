5ed18f36d27a1835eff4c225ce4e131221ae998d
mfina94 <matthawkeyefina@gmail.com>
2015-11-06 16:16:56 +0000

    sessions controller spec done besides create, buildings controller spec done besides update

--- apartments/spec/controllers/sessions_controller_spec.rb
+++ apartments/spec/controllers/sessions_controller_spec.rb
@@ -2,5 +2,27 @@ require 'rails_helper'
 require 'users_controller'
 
 RSpec.describe SessionsController, type: :controller do
-
+    describe "creating a new user account" do
+        it "renders the new template for the user" do
+            get :new
+            expect(response).to render_template("new")
+        end
+        
+        it "redirects the user the buildings homepage upon logging in" do
+            #post :create, {} {session[:email] = "test@123.yahoo.com", session[:password]= "12341234"}
+            #expect(response).to redirect_to(buildings_path)
+        end
+        
+        it 'redirects the user to the buildings new page if verification failed' do
+            #get :create, {}, {}
+            #expect(response).to redirect_to(new_session_path)
+        end
+    end
+    
+    describe "logging out of account" do
+        it "should clear the session cookie" do
+           get :destroy
+           expect(response).to redirect_to(buildings_path)
+        end
+    end
 end
--- apartments/spec/factories/buildings.rb
+++ apartments/spec/factories/buildings.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :building do
-    
+    email  "123@yahoo.com"
+    password  "test12345"
   end
 
 end
