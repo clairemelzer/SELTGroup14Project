32c5cc87033471beebac7d664c6f9e59dd4c395a
jzlantz <jzlantz@gmail.com>
2015-11-06 03:46:48 +0000

    Replace should with expect in Rspec to avoid deprecation warnings

--- apartments/spec/models/user_spec.rb
+++ apartments/spec/models/user_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
  it "has a valid Factory" do
-     create(:user).should be_valid
+     expect(create(:user)).to be_valid
  end
  
  it "Is invalid without email" do
-     build(:user, email: nil).should_not be_valid
+     expect(build(:user, email: nil)).to_not be_valid
  end
  it "Is invalid without password" do
-     build(:user, password: nil).should_not be_valid
+     expect(build(:user, password: nil)).to_not be_valid
  end
  it "creates a session token" do
      user = create(:user)
