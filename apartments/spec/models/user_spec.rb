036e9289e95eae84cebb471f6ae238dd6fe50901
jzlantz <jzlantz@gmail.com>
2015-11-06 04:04:26 +0000

    Added too short password and too long name Rspec validations

--- apartments/spec/models/user_spec.rb
+++ apartments/spec/models/user_spec.rb
@@ -11,17 +11,22 @@ RSpec.describe User, type: :model do
  it "Is invalid without password" do
      expect(build(:user, password: nil)).to_not be_valid
  end
-  it "Is invalid without password  confirmation" do
+  it "Is invalid without password confirmation" do
      expect(build(:user, password_confirmation: nil)).to_not be_valid
  end
   it "Is invalid without name" do
      expect(build(:user, name: nil)).to_not be_valid
  end
- 
  it "creates a session token" do
      user = create(:user)
      expect(user.session_token).to be_truthy
  end
- 
+ it "is invalid with too short of password" do
+     expect(build(:user, password: "hi")).to_not be_valid
+ end
+ it "is invalid with too long of name" do
+  expect(build(:user, name: SecureRandom.base64(50) )).to_not be_valid
+end
+
  
 end
