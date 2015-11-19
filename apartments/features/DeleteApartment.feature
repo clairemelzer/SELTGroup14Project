8ed325fc7a7e94e5e0ce66e442e7a410cf575a2c
clairemelzer <claire-melzer@uiowa.edi>
2015-11-19 02:51:26 +0000

    added some cucumber tests

--- apartments/features/DeleteApartment.feature
+++ apartments/features/DeleteApartment.feature
@@ -1,6 +1,14 @@
 Feature: User should be able to delete apartment
   
 Scenario: Authorized User wants to delete apartment
-  
+  Given I have created a new user with name "Claire" and email "clairetest@email.com" and password "hello1" and confirmation "hello1"
+  Given I have created a new session with email "clairetest@email.com" and password "hello1"
+  Given I have added a building with address "19 E Burlington St" and management "Apartments Downtown"
+  And I have visited the Details about "19 E Burlington St" page 
+  And I have clicked on add new apartment
+  When I have added an apartment with number "101"
+  And I have visited the more about apartment number "101" page 
+  And I have clicked on delete
+  Then I should not see an apartment list entry with number "101" 
   
 Scenario: Unauthorized User wants to delete apartment
--- apartments/features/step_definitions/apartment_steps.rb
+++ apartments/features/step_definitions/apartment_steps.rb
@@ -5,10 +5,19 @@
    When /^I have clicked on add review$/ do 
    click_on "Add review for this apartment"
  end
+ 
+    When /^I have clicked on edit$/ do 
+   click_on "Edit This Apartment"
+ end
   
  When /^I have added an apartment with number "(.*?)"$/ do |number|
   fill_in 'Apartment Number', :with => number
   click_button 'Add Apartment'
+ end
+ 
+  When /^I have updated the apartment with number "(.*?)"$/ do |number|
+  fill_in 'Apartment Number', :with => number
+  click_button 'Update Apartment'
  end
 
  Then /^I should see an apartment list entry with number "(.*?)"$/ do |number| 
@@ -20,6 +29,17 @@
      end
    end  
   expect(result).to be_truthy
+ end
+ 
+  Then /^I should not see an apartment list entry with number "(.*?)"$/ do |number| 
+   result=false
+   all("tr").each do |tr|
+     if tr.has_content?(number)
+       result = false
+       break
+     end
+   end  
+  expect(result).to be_falsey
  end
  
   When /^I have visited the more about apartment number "(.*?)" page$/ do |number|
